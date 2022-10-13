f7027656b20a153a7291838e09a8dfdd17bb5712
Lok <loklokj@gmail.com>
2022-10-13 00:28:31 -0400

    remove js

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,8 @@
-const  useEffect = require("react").useEffect
-
-
 function HomePage() {
-    useEffect(()=>{
-    window.location.href = 'https://sites.duke.edu/marcdryser/'
-    },[])
 
-    return <div>Forwarding... if you are not being redirected please click <a href="https://sites.duke.edu/marcdryser/">here to visit Marc D. Ryser 's homepage.</a></div>
+    return<head>
+    <meta httpEquiv='refresh' content='0; URL=https://sites.duke.edu/marcdryser/'></meta>
+  </head>
   }
   
   export default HomePage
